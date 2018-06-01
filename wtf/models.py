3b357462d03d3d34f56b73c29f3067a96502a323
Till Dettmering <prinzipiell@gmail.com>
2018-06-01 23:24:26 +0200

    Tag: Replace CharField with ManyToMany

--- wtf/models.py
+++ wtf/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 #from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 class WTF(models.Model):
     created_timestamp = models.DateTimeField(auto_now_add=True)
     content = models.CharField(max_length=500)
-    tags = models.CharField(max_length=100)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
     short = models.CharField(max_length=8)
 
     def __str__(self):
-        return self.email
+        return self.content
