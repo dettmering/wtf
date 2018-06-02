499a9c494a47b39416777a6fd779fed768e8c02a
Till Dettmering <prinzipiell@gmail.com>
2018-06-02 11:35:38 +0200

    ssl redirect

--- wtf/settings.py
+++ wtf/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # Change 'default' database configuration with $DATABASE_URL.
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 
+# Activate SSL
+SECURE_SSL_REDIRECT = boolenv('DJANGO_SSL_REDIRECT', False)
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
